dec74f64de2feba493e1d7c27e0b88bb27c28ac0
Dan <dannguyen90@gmail.com>
2018-05-16 21:02:05 -0400

    change variable name for clarity

--- daily_coding_problem_4.js
+++ daily_coding_problem_4.js
@@ -9,52 +9,53 @@
 // You can modify the input array in-place.
 
 
-function missingPositiveInt(array) {
+function missingPositiveInt(integers) { // parameter is an array
 
   // use the array's indices to track which integers exist in the array,
   // disregarding any number < 1 (not a positive int) or greater than the array
   // length (impossible to be the lowest missing int).
-  for (var i = 0; i < array.length; i++) {
+  for (var i = 0; i < integers.length; i++) {
 
     // if element is equal to its index + 1, continue.
-    if (array[i] === i + 1) {
+    if (integers[i] === i + 1) {
       continue;
 
     // if element is not a positive int or is greater than the array's length,
     // change value to null.
-    } else if (array[i] < 1 || array[i] > array.length) {
-      array[i] = null;
+    } else if (integers[i] < 1 || integers[i] > integers.length) {
+      integers[i] = null;
 
     // otherwise, move the element to its appropriate position, using the
     // var onDeck to track what needs to be moved next. Repeat until the onDeck
     // int does not need to be moved.
     } else {
-      var onDeck = array[i];
-      array[i] = null;
+      var onDeck = integers[i];
+      integers[i] = null;
 
-      while (onDeck > 0 && onDeck <= array.length) {
+      while (onDeck > 0 && onDeck <= integers.length) {
 
-        if (array[onDeck - 1] === onDeck) {
+        if (integers[onDeck - 1] === onDeck) {
           break;
         }
 
-        var tmp = array[onDeck - 1];
-        array[onDeck - 1] = onDeck;
+        // swap onDeck with integers[onDeck - 1]
+        var tmp = integers[onDeck - 1];
+        integers[onDeck - 1] = onDeck;
         onDeck = tmp;
       }
     }
   }
 
   // the first null value will be the lowest missing positive int
-  for (var i = 0; i < array.length; i++) {
+  for (var i = 0; i < integers.length; i++) {
 
-    if (array[i] === null) {
+    if (integers[i] === null) {
       return i + 1;
     }
   }
 
   //if no null values are found, return the next positive int
-  return array.length + 1;
+  return integers.length + 1;
 }
 
 
