5d881a1a5db48304a0a9292e3f6add64c1603d45
Dan <dannguyen90@gmail.com>
2018-05-18 16:30:20 -0400

    change comments

--- daily_coding_problem_5.py
+++ daily_coding_problem_5.py
@@ -1,5 +1,3 @@
-# This problem was asked by Jane Street.
-#
 # cons(a, b) constructs a pair, and car(pair) and cdr(pair) returns the first
 # and last element of that pair. For example, car(cons(3, 4)) returns 3, and
 # cdr(cons(3, 4)) returns 4.
@@ -9,6 +7,7 @@
 # def cons(a, b):
 #     return lambda f : f(a, b)
 # Implement car and cdr.
+
 
 def cons(a, b):
     return lambda f : f(a, b)
