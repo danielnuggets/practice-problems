47faea35670867689a499afed59c904d8a099327
Dan <dannguyen90@gmail.com>
2018-06-05 12:10:28 -0400

    change solution to solve in linear time

--- daily_coding_problem_7.js
+++ daily_coding_problem_7.js
@@ -4,90 +4,86 @@
 // For example, the message '111' would give 3, since it could be decoded as
 // 'aaa, 'ka', and 'ak'.
 
-function encodingCounter(s) {
-  // check if first character is 0
+
+function decodingCounter(s) {
+  let countTwoBefore = 1;
+  let countOneBefore = 1;
+  let countCurrent;
+
   if (s.charAt(0) === '0') {
     return 0;
-  }
-
-  // check for any invalid 0's
-  for (let i = 1; i < s.length; i++) {
-    if (s.charAt(i) === '0' && !['1', '2'].includes(s.charAt(i - 1))) {
-      return 0;
-    }
-  }
-
-  return encodingCounterHelper(s);
-}
-
-function encodingCounterHelper(msg) {
-  let firstTwo = msg.charAt(0) + msg.charAt(1);
-
-  if (msg.length < 2) {
+  } else if (s.length < 2) {
     return 1;
-
-  // if first two chars are 11 - 26, divide into two paths - one
-  // interpreting as 2 single digits, and the other as 1 double digit.
-} else if (firstTwo > 10 && firstTwo < 27 && msg.charAt(1) != '0' && msg.charAt(2) != '0') {
-    return encodingCounterHelper(msg.substr(1)) + encodingCounterHelper(msg.substr(2));
-
-  } else {
-    return encodingCounterHelper(msg.substr(1));
   }
-}
 
+  for (let i = 1; i < s.length; i++) {
+    let prev = s.charAt(i - 1);
+    let curr = s.charAt(i);
+    let next = s.charAt(i + 1);
+
+    // check for invalid 0
+    if (curr === '0' && !['1','2'].includes(prev)) {
+      return 0;
+    // check for ambiguous double digit number
+  } else if (prev+curr > 10 && prev+curr < 27 && curr !== '0' && next !== '0') {
+      countCurrent = countTwoBefore + countOneBefore;
+    } else {
+      countCurrent = countOneBefore;
+    }
+
+    countTwoBefore = countOneBefore;
+    countOneBefore = countCurrent;
+  }
+
+  return countCurrent;
+}
 
 // tests
 
 var msg1 = "111";
-if (encodingCounter(msg1) === 3) {
+if (decodingCounter(msg1) === 3) {
   console.log("pass");
 } else {
   console.log("fail");
 }
 
 var msg2 = "2373149201525920";
-if (encodingCounter(msg2) === 16) {
+if (decodingCounter(msg2) === 16) {
   console.log("pass");
 } else {
   console.log("fail");
 }
 
 var msg3 = "12121";
-
-if (encodingCounter(msg3) === 8) {
+if (decodingCounter(msg3) === 8) {
   console.log("pass");
 } else {
   console.log("fail");
 }
 
 var msg4 = "99999999";
-
-if (encodingCounter(msg4) === 1) {
+if (decodingCounter(msg4) === 1) {
   console.log("pass");
 } else {
   console.log("fail");
 }
 
 var msg5 = "12";
-
-if (encodingCounter(msg5) === 2) {
+if (decodingCounter(msg5) === 2) {
   console.log("pass");
 } else {
   console.log("fail");
 }
 
 var msg6 = "0"
-
-if (encodingCounter(msg6) === 0) {
+if (decodingCounter(msg6) === 0) {
   console.log("pass");
 } else {
   console.log("fail");
 }
 
 var msg7 = "91362931004"
-
-if (encodingCounter(msg7) === 0) {
+if (decodingCounter(msg7) === 0) {
   console.log("pass");
 } else {
   console.log("fail");
